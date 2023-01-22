61c5217255da2a802b28d593ad2c1617cf2386db
CJskii <czareczarek3@gmail.com>
2023-01-22 13:15:04 +0000

    Submit name and title

--- cv-project/src/Components/General/General.js
+++ cv-project/src/Components/General/General.js
@@ -3,44 +3,78 @@ import Name from "./Name";
 import Underline from "./Underline";
 import Title from "./Title";
 import "./General.css";
-import { FaEdit } from "react-icons/fa";
 
 class General extends Component {
   constructor() {
     super();
     this.state = {
       user: { name: "MICHAEL JORDAN", title: "CREATIVE DIRECTOR" },
-      name: null,
-      title: null,
+      showEditBtn: false,
+      showForm: false,
     };
 
-    this.nameChange = this.handleNameChange.bind(this);
-    this.titleChange = this.handleTitleChange.bind(this);
+    this.showEditBtn = this.showEditBtn.bind(this);
+    this.editForm = this.editForm.bind(this);
+    this.showForm = this.showForm.bind(this);
+    this.submitForm = this.handleFormSubmit.bind(this);
   }
 
-  handleNameChange(e) {
-    this.setState((prevState) => {
-      let user = this.state.user;
-      user.name = e.target.value;
-      return { user };
+  showEditBtn() {
+    return (
+      <button className="editBtn btn" onClick={this.showForm}>
+        EDIT
+      </button>
+    );
+  }
+
+  showForm() {
+    this.setState({ showForm: true });
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+    let name = document.querySelector(".name-input");
+    let title = document.querySelector(".title-input");
+    this.setState({
+      user: {
+        name: name.value.toUpperCase(),
+        title: title.value.toUpperCase(),
+      },
+      showForm: false,
     });
   }
 
-  handleTitleChange(e) {
-    this.setState((prevState) => {
-      let user = this.state.user;
-      user.title = e.target.value;
-      return { user };
-    });
+  editForm() {
+    return (
+      <form
+        className="name-form"
+        autoComplete="off"
+        onSubmit={(e) => this.submitForm(e)}
+      >
+        <label htmlFor="name">Enter your name</label>
+        <input id="name" className="name-input" type="text" />
+        <label htmlFor="title">Enter your title</label>
+        <input id="title" className="title-input" type="text" />
+        <button type="submit" className="submitBtn btn">
+          Submit
+        </button>
+      </form>
+    );
   }
 
   render() {
     const { user } = this.state;
     return (
-      <div className="wrapper">
-        <Name user={user} nameChange={this.nameChange} />
+      <div
+        className="wrapper"
+        onMouseEnter={() => this.setState({ showEditBtn: true })}
+        onMouseLeave={() => this.setState({ showEditBtn: false })}
+      >
+        <Name user={user} />
         <Underline />
-        <Title user={user} titleChange={this.titleChange} />
+        <Title user={user} />
+        {this.state.showEditBtn && this.showEditBtn()}
+        {this.state.showForm && this.editForm()}
       </div>
     );
   }
--- cv-project/src/Components/General/Title.js
+++ cv-project/src/Components/General/Title.js
@@ -1,59 +1,20 @@
 import React, { Component } from "react";
-import { FaCheck } from "react-icons/fa";
 
 class Title extends Component {
   constructor(props) {
     super();
-
-    this.state = {
-      editing: false,
-    };
-
-    this.editing = this.handleClickTitle.bind(this);
-    this.saveTitle = this.handleSaveTitle.bind(this);
-  }
-
-  handleClickTitle() {
-    this.setState({
-      editing: true,
-    });
-  }
-
-  handleSaveTitle() {
-    this.setState({
-      editing: false,
-    });
   }
 
   render() {
-    const { user, titleChange } = this.props;
-    const { editing } = this.state;
+    const { user } = this.props;
 
-    if (editing === true) {
-      return (
-        <div className="title-wrapper">
-          <div className="title-container">
-            <input
-              className="title-input"
-              value={user.title}
-              type="text"
-              onChange={(e) => titleChange(e)}
-            />
-            <div className="icon">
-              <FaCheck onClick={this.saveTitle} />
-            </div>
-          </div>
+    return (
+      <div className="title-wrapper">
+        <div className="title" onClick={this.editing}>
+          {user.title}
         </div>
-      );
-    } else {
-      return (
-        <div className="title-wrapper">
-          <div className="title" onClick={this.editing}>
-            {user.title}
-          </div>
-        </div>
-      );
-    }
+      </div>
+    );
   }
 }
 
